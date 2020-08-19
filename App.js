7f98bdedc5f6c72935e37fbea6cb31f19ff410d7
adilcjr <adil.calomeno.jr@gmail.com>
2020-08-19 10:55:10 -0300

    Navigator added to App.js

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import RootNavigator from './src/navigation/RootNavigator';
+
+import firebaseKeys from './src/config/firebaseKeys'
+
+import * as firebase from 'firebase'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseKeys);
+  }
+
+  return (
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
+  )
+}
